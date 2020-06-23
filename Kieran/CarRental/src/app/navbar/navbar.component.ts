d72db588caa8eafd88a98c5a7781d9ba09bedd7c
Aki <kieran.mortimer@gmail.com>
2020-06-23 11:33:10 +0200

    donate logic added

--- Kieran/CarRental/src/app/navbar/navbar.component.ts
+++ Kieran/CarRental/src/app/navbar/navbar.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  totalDonated: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  donate(amount: number) {
+    this.totalDonated += amount;
+    console.log(this.totalDonated);
   }
-
 }
